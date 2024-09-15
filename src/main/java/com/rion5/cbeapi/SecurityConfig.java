e7753bb2c80e4e6530ac6b062de5ea90e4a3ad31
rion5 <rion5@MyPro.local>
2024-09-16 08:55:36 +0900

    add cors

--- src/main/java/com/rion5/cbeapi/SecurityConfig.java
+++ src/main/java/com/rion5/cbeapi/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig {
         return new WebMvcConfigurer(){
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry){
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000","http://166.104.191.113:3000","http://166.104.191.59:3000","http://166.104.191.59")
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000","http://166.104.191.113:3000","http://166.104.191.59:3000","http://192.168.0.39:3000")
                 .allowedMethods(HttpMethod.GET.name(), HttpMethod.HEAD.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(), HttpMethod.DELETE.name());
             }
         };
